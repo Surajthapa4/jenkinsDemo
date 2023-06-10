2276ec0919e2197d2036141debf2e231d7d19eb6
Suraj Thapa <suraj.thapa@mindbowser.com>
2023-06-10 12:18:34 +0530

    stage changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Develop Branch
+          Learn React Staging Branch
         </a>
       </header>
     </div>
